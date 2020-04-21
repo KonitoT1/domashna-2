40fd2e445485579241be27ef42dd5d319977fcd4
KonitoT1 <63659415+KonitoT1@users.noreply.github.com>
2020-04-21 23:40:40 +0300

    Update sds3.java

--- sds3.java
+++ sds3.java
@@ -1,45 +1,47 @@
 
 public class sds3
 {
-    public static void main(final String[] args) {
-        for (int i = 8; i >= 0; --i) {
+   public static void main(String [] args){
+
+        for (int i = 8; i >= 0;i-- ){
+
+
             switch (i) {
-                case 8: {
-                    System.out.println("#8: \u0417\u0434\u0440\u0430\u0432\u0435\u0439, \u041c\u0438\u043c\u0438!");
+                case 8:
+                    System.out.println("#8: Здравей, Мими!");
                     break;
-                }
-                case 7: {
-                    System.out.println("#7: \u0417\u0434\u0440\u0430\u0432\u0435\u0439, \u0412\u0430\u043d\u044f!");
+
+                case 7:
+                    System.out.println("#7: Здравей, Ваня!");
                     break;
-                }
-                case 6: {
-                    System.out.println("#6: \u0417\u0434\u0440\u0430\u0432\u0435\u0439, \u041f\u0435\u0442\u044f!");
+
+                case 6:
+                    System.out.println("#6: Здравей, Петя!");
                     break;
-                }
-                case 5: {
-                    System.out.println("#5: \u0417\u0434\u0440\u0430\u0432\u0435\u0439, \u0418\u0432\u0430\u043d\u0430! ");
+
+                case 5:
+                    System.out.println("#5: Здравей, Ивана! ");
                     break;
-                }
-                case 4: {
-                    System.out.println("#4: \u0417\u0434\u0440\u0430\u0432\u0435\u0439, \u0422\u0430\u043d\u044f!");
+
+                case 4:
+                    System.out.println("#4: Здравей, Таня!");
                     break;
-                }
-                case 3: {
-                    System.out.println("#3: \u0417\u0434\u0440\u0430\u0432\u0435\u0439, \u041d\u0438\u043a\u0438!");
+
+                case 3:
+                    System.out.println("#3: Здравей, Ники!");
                     break;
-                }
-                case 2: {
-                    System.out.println("#2: \u0417\u0434\u0440\u0430\u0432\u0435\u0439, \u041f\u0435\u043f\u0438!");
+
+                case 2:
+                    System.out.println("#2: Здравей, Пепи!");
                     break;
-                }
-                case 1: {
-                    System.out.println("#1: \u0417\u0434\u0440\u0430\u0432\u0435\u0439, \u0413\u043e\u0448\u043e!");
+
+                case 1:
+                    System.out.println("#1: Здравей, Гошо!");
                     break;
-                }
-                case 0: {
+
+                case 0:
                     System.out.println("#0: Auch.");
                     break;
-                }
             }
         }
     }
