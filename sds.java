ff28977ecc31ba7695a0914292e4185b2f8811a3
KonitoT1 <63659415+KonitoT1@users.noreply.github.com>
2020-04-21 23:32:20 +0300

    Add files via upload

---
+++ sds.java
@@ -0,0 +1,15 @@
+// 
+// Decompiled by Procyon v0.5.36
+// 
+
+public class sds
+{
+    public static void main(final String[] args) {
+        final int a = 2;
+        final int b = 6;
+        final int p = 2 * (a + b);
+        final int s = a * b;
+        System.out.println("Perimeter is: " + p);
+        System.out.println("Area is: " + s);
+    }
+}
---
+++ sds2.java
@@ -0,0 +1,58 @@
+// 
+// Decompiled by Procyon v0.5.36
+// 
+
+public class sds2
+{
+    public static void main(final String[] args) {
+        final String January = "january";
+        final String February = "february";
+        final String March = "march";
+        final String April = "april";
+        final String May = "may";
+        final String June = "june";
+        final String July = "july";
+        final String August = "august";
+        final String September = "september";
+        final String October = "october";
+        final String November = "november";
+        final String December = "december";
+        final String EnterMonth = "february";
+        if (January == EnterMonth) {
+            System.out.println("31 days");
+        }
+        else if (February == EnterMonth) {
+            System.out.println("28 days");
+        }
+        else if (March == EnterMonth) {
+            System.out.println("31 days");
+        }
+        else if (April == EnterMonth) {
+            System.out.println("30 days");
+        }
+        else if (May == EnterMonth) {
+            System.out.println("31 days");
+        }
+        else if (June == EnterMonth) {
+            System.out.println("30 days");
+        }
+        else if (July == EnterMonth) {
+            System.out.println("31 days");
+        }
+        else if (August == EnterMonth) {
+            System.out.println("31 days");
+        }
+        else if (September == EnterMonth) {
+            System.out.println("30 days");
+        }
+        else if (October == EnterMonth) {
+            System.out.println("31 days");
+        }
+        else if (November == EnterMonth) {
+            System.out.println("30 days");
+        }
+        else if (December == EnterMonth) {
+            System.out.println("31 days");
+        }
+    }
+}
